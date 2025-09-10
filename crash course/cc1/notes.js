4d7e163caef33d0a073599098d6daa59f1740018
Ashish Shukla <akshkgd@gmail.com>
2025-09-10 21:16:00 +0530

    wd

---
+++ crash course/cc1/notes.js
@@ -0,0 +1,7 @@
+// box model
+// p => padding
+// m=> margin => mx and my
+// colors
+// flex
+// positions
+// responsive design
--- crash course/react-intro/src/components/Demo.jsx
+++
@@ -1,8 +0,0 @@
-function Demo(abc){
-    return(
-        <>
-            <h1 className="mt-5 text-center">{abc.title}</h1>
-        </>
-    )
-}
-export default Demo;
